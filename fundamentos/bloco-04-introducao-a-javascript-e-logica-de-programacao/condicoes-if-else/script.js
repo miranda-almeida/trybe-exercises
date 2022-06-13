bb6411a96fcf9ead59d21ac5a6a4ee16a8530140
miranda <magui@duck.com>
2022-06-13 19:29:58 -0300

    condições if/else

---
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/condicoes-if-else/script.js
@@ -0,0 +1,9 @@
+const nota = 70;
+
+if (nota >= 80) {
+    console.log("Parabéns pela aprovação!")
+} else if (nota < 80 && nota >= 60) {
+    console.log("Você está na nossa lista de espera")
+} else {
+    console.log("Você não recebeu aprovação")
+}

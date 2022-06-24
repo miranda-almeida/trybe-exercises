663b81c125008eb022edc07ff893f999c4f767e1
miranda <magui@duck.com>
2022-06-23 22:40:54 -0300

    marcando exercícios de array e loops pendentes

--- fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/exercicios-02_arrays-e-loops.js
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/exercicios-02_arrays-e-loops.js
@@ -143,7 +143,7 @@ for(array = 1; array <= 25; array += 1) {
     console.log(array);
 }
 
-// utilizando o array criado no exercício anterior imprima o resultado da divisão de cada um dos elementos por 2
+// PENDENTE utilizando o array criado no exercício anterior imprima o resultado da divisão de cada um dos elementos por 2
 
 array = [];
 division = 0;
@@ -155,3 +155,14 @@ for(array = 1; array <= 25; array += 1) {
 }
 
 console.log(division);
+
+// PENDENTE bônus
+
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+
+// 1) ordene o array numbers em ordem crescente e imprima seus valores;
+
+// 2) ordene o array numbers em ordem decrescente e imprima seus valores;
+
+// 3) agora crie um novo array a partir do array numbers, sem perdê-lo. Cada valor do novo array deverá ser igual ao valor correspondente no array numbers multiplicado pelo seguinte. Por exemplo: o primeiro valor do novo array deverá ser 45, pois é a multiplicação de 5 (primeiro valor) e 9 (valor seguinte). Já o segundo valor do novo array deverá ser 27, pois é a multiplicação de 9 (segundo valor) e 3 (valor seguinte), e assim por diante. Caso não haja próximo valor, a multiplicação deverá ser feita por 2. Faça isso utilizando o for e o método push. O resultado deve ser o array: [45, 27, 57, 1330, 560, 800, 200, 70, 945, 54]

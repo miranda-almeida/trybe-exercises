311f994808dda1184184f977f5cf5c49a0fc5827
miranda <magui@duck.com>
2022-06-23 22:47:41 -0300

    adiciona exercícios pendentes de lógica de programação e algoritmos

--- fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/exercicios-02_arrays-e-loops.js
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/exercicios-02_arrays-e-loops.js
@@ -119,7 +119,7 @@ console.log(menorValor);
 
 // descubra quantos valores ímpares existem no array e imprima o resultado. Caso não exista nenhum, imprima a mensagem: "nenhum valor ímpar encontrado"; 
 
-let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+//let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
 let impares = 0;
 
---
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/exercicios-03_logica-e-algoritmos.js
@@ -0,0 +1,24 @@
+// 1) o fatorial é a multiplicação de um número natural pelos seus antecessores, exceto o zero. Por exemplo:
+
+// o fatorial é representado pelo sinal !
+// ! = 4 x 3 x 2 x 1 = 24
+
+// com base nessas informações, crie um algoritmo que retorne o fatorial de 10.
+
+
+
+// 2) agora, desenvolva um algoritmo que é capaz de inverter uma palavra. por exemplo, a palavra "banana" seria invertida para "ananab". utilize a string abaixo como exemplo, depois troque por outras para verificar se seu algoritmo está funcionando corretamente.
+
+// let word = 'tryber';
+
+
+
+// 3) considere o array de strings abaixo:
+
+// let array = ['java', 'javascript', 'python', 'html', 'css'];
+
+// escreva dois algoritmos: um que retorne a maior palavra deste array e outro que retorne a menor. considere o número de caracteres de cada palavra.
+
+
+
+// 4) um número primo é um número inteiro maior do que 1 que possui somente dois divisores, ou seja, é divisível por 1 e por ele mesmo. Sabendo disso, escreva um algoritmo que retorne o maior número primo entre 2 e 50.

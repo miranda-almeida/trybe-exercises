35506e4de591cb955bb4778493c5afb65e08946a
miranda <magui@duck.com>
2022-06-26 14:57:16 -0300

    primeiro exercício após vídeo de .getElemntById

---
+++ fundamentos/bloco-05-dom-eventos-e-web-storage/exercicios-01_dom-e-seletores.js
@@ -0,0 +1,12 @@
+// -------------------------------------------------------------------------------------------------------------------- GET ELEMENT BY ID
+// recupera o elemento que contém o título da página e faça algo com ele, como alterá-lo para o nome do seu filme favorito. 
+document.getElementById('page-title').innerText = 'Amélie Poulain';
+
+//  recupere o segundo parágrafo e use sua criatividade para alterá-lo.
+document.getElementById('second-paragraph').innerText = 'Alterando segundo parágrafo';
+
+// por fim, recupere o subtítulo e altere-o também. 
+document.getElementById('subtitle').style.color = "red";
+
+// altera a cor do primeiro parágrafo.
+document.getElementById('paragraph').style.color = "pink";

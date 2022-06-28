e8043b040d327fa9c50e75e6f3c54c991d9c6749
miranda <magui@duck.com>
2022-06-28 14:34:20 -0300

    correções lint

--- fundamentos/bloco-05-dom-eventos-e-web-storage/query-selectors/exercicios-02_query-selectors.js
+++ fundamentos/bloco-05-dom-eventos-e-web-storage/query-selectors/exercicios-02_query-selectors.js
@@ -1,4 +1,4 @@
 let backgroundHeader = document.querySelector('header')[0];
-for (let index = 0; index < backgroundHeader.length; index += 1;) {
+for (let index = 0; index < backgroundHeader.length; index += 1) {
     backgroundHeader.style.color = 'green';
 }
